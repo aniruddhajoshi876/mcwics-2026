e0df16e4fe8672405caff325bad52814ab473de5
scakes <109105997+scakes@users.noreply.github.com>
2026-02-07 16:14:30 -0500

    added another div

--- prompt-pages/apps/web/src/scripts/personalscript.js
+++ prompt-pages/apps/web/src/scripts/personalscript.js
@@ -1,20 +1,21 @@
 
 function drag_start(event) {
-    var style = window.getComputedStyle(event.target, null);
+    var style = window.getComputedStyle(event.target, null); // gets all styles of element being dragged
     var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
-    event.dataTransfer.setData("text/plain", str);
+    event.dataTransfer.setData("text/plain", str); // saves as a string
+    // index 0 -> horz. offset; 1 -> vertical offset, 2 -> event ID
 }
 
 function drop(event) {
     var offset = event.dataTransfer.getData("text/plain").split(',');
     var dm = document.getElementById(offset[2]);
-    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px'; // finds new location
     dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
     event.preventDefault();
     return false;
 }
 
 function drag_over(event) {
-    event.preventDefault();
+    event.preventDefault(); // allows element to recieve drops
     return false;
 }
