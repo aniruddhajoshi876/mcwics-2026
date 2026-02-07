04a117bfde78722e1079ad1fa34e9d0c952fece1
ilgaz <ilgazalsancak2006@gmail.com>
2026-02-07 18:18:39 -0500

    started read file function

--- prompt-pages/backend/utils/read_data.py
+++ prompt-pages/backend/utils/read_data.py
@@ -1,6 +1,17 @@
-def find_room(room_id):
-    with open("mcwics-2026/prompt-pages/backend/room_data.json", "r") as file:
+from pathlib import Path
+
+BASE = Path(__file__).resolve().parent
+
+filepath = BASE.parent / "room_data.json"
+
+
+def find_room():
+    with open(filepath, "r") as file:
         data = file.read()
-        
+    return data
+
+if __name__ == "__main__":
+    print(find_room())
+
         
 
