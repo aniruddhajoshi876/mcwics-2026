edc41159a90ab37e3945588d33401f24bc4966a4
scakes <109105997+scakes@users.noreply.github.com>
2026-02-07 15:24:02 -0500

    divs can now move!

--- prompt-pages/apps/web/src/scripts/personalscript.js
+++ prompt-pages/apps/web/src/scripts/personalscript.js
@@ -2,11 +2,11 @@
 function drag_start(event) {
     var style = window.getComputedStyle(event.target, null);
     var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
-    event.dataTransfer.setData("Text", str);
+    event.dataTransfer.setData("text/plain", str);
 }
 
 function drop(event) {
-    var offset = event.dataTransfer.getData("Text").split(',');
+    var offset = event.dataTransfer.getData("text/plain").split(',');
     var dm = document.getElementById(offset[2]);
     dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
     dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
