628f14ed72b9b0938d54fb020ac4f715f18cd5eb
ilgaz <ilgazalsancak2006@gmail.com>
2026-02-07 16:05:29 -0500

    added create/enter room feature

--- prompt-pages/backend/main.py
+++ prompt-pages/backend/main.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from routes.rooms import bp as rooms_bp
 
+#setup flask app, specify template and static folders
 app = Flask(__name__, template_folder="../apps/web/src/pages/templates", static_folder="../apps/web/src/static")
 
 
 app.register_blueprint(rooms_bp)
 
-
+#main route
 @app.route("/")
 def home():
     return render_template("index.html")
 
+#create a new room with given room id
+@app.route("/create_room", methods=["POST"])
+def create_room():
+    room_id = request.form.get("room-id").strip()
+    print(room_id)
+    return render_template("index.html")
+
+#enter pre-existing room with given room id
+@app.route("/enter_room", methods=["POST"])
+def enter_room():
+    room_id = request.form.get("room-id").strip()
+    print(room_id)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
