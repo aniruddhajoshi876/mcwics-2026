d9db72642b4188a2bea17136f79b9c83be3562b8
ilgaz <ilgazalsancak2006@gmail.com>
2026-02-07 20:49:19 -0500

    data manage

--- prompt-pages/backend/utils/manage_data.py
+++ prompt-pages/backend/utils/manage_data.py
@@ -19,10 +19,24 @@ def find_room(room_id):
 def manage_containers(containers: list):
     for container in containers:
         print(f"Caption: {container['caption']}")
+
+def create_room(room_id):
+    with open(filepath, "r") as file:
+        file_read = file.read()
+        data_dictionary = json.loads(file_read)
+        old_length = len(data_dictionary)
+        print(len(data_dictionary))
+        data_dictionary[room_id] = {"containers": []}
+        new_length = len(data_dictionary)
+        if new_length > old_length:
+            with open(filepath, "w") as file:
+                json.dump(data_dictionary, file)
+
     
 
 if __name__ == "__main__":
-    print(find_room("testid"))
+    #print(find_room("testid"))
+    create_room("152516")
 
         
 
