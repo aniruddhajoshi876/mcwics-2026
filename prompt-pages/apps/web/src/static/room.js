c64a7f331b7d98da1c5376f203e9ee9e44e2a205
ilgaz <ilgazalsancak2006@gmail.com>
2026-02-08 07:07:22 -0500

    updated room loading

--- prompt-pages/apps/web/src/static/room.js
+++ prompt-pages/apps/web/src/static/room.js
@@ -12,9 +12,39 @@ addButtonEl.addEventListener('click', () => {
 
 })
 
-function loadData() {
+function loadData(posts) {
+    console.log(posts);
+
+    posts.forEach(post => {
+
+    console.log(post);
+    const postHTML = ` <div id="box1" class="box" draggable="true">
+        <span class="name" contenteditable="true">user</span>
+        <span class="date" id="date">${post.date}</span>
+
+        <div class="image"> 
+            <input type="file">     
+        </div>
+        <div class="caption" contenteditable="true">${post.caption}</div>
+    </div>
     
+    `
+
+    postSpawnEl.insertAdjacentHTML('beforeend', postHTML);
+
+    const newPost = postSpawnEl.lastElementChild;
+
+    newPost.id = 'post_' + Date.now();
+    newPost.ondragstart = drag_start;
+
+});
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadData(posts)
+});
+
+
 
 function makePost(postData) {
      const date = new Date();
