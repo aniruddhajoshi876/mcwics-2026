84e551eef4bfd575e1f32bc31d5d85adbfe58ecf
Aniruddha Joshi <aniruddha.jaguar@gmail.com>
2026-02-07 21:21:48 -0500

    minor changes to events.py

--- prompt-pages/backend/realtime/events.py
+++ prompt-pages/backend/realtime/events.py
@@ -9,14 +9,14 @@ from save import save_room, load_data
 def on_container_add(data):
     room_id = data.get('roomId')
     content = data.get('content')
-    container_id = data.get('container_id')
+    container_id = data.get('containerId')
 
     if not room_id or content is None:
         return
 
     emit(
         "container_added", 
-        {'container_id': container_id,
+        {'containerId': container_id,
          'content': content
          },
          room=room_id,
@@ -27,7 +27,7 @@ def on_container_add(data):
 @socketio.on('container_update')
 def on_container_update(data):
     room_id = data.get('roomId')
-    container_id = data.get('container_id')
+    container_id = data.get('containerId')
     content = data.get('content')
 
     if not room_id or content is None or container_id is None:
@@ -36,7 +36,7 @@ def on_container_update(data):
 
     emit(
         "container_updated", 
-        {'container_id': container_id,
+        {'containerId': container_id,
          'content': content
          },
          room=room_id,
@@ -46,20 +46,20 @@ def on_container_update(data):
 @socketio.on('container_delete')
 def on_container_delete(data):
     room_id = data.get('roomId')
-    container_id = data.get('container_id')
+    container_id = data.get('containerId')
 
     if not room_id or container_id is None:
         return
     emit(
         "container_deleted", 
-        {'container_id': container_id,
+        {'containerId': container_id,
          },
          room=room_id,
          include_self=False
     )
 
 @socketio.on('save_state')
-def save_state(data):
+def on_save_state(data):
     room_id = data.get('room_id') or data.get('roomId')
     state = data.get('state')
 
@@ -68,21 +68,25 @@ def save_state(data):
     try:
         save_room(room_id, state)
     except Exception as e:
-        print('Error while saving...')
+        print('Error while saving...',e)
 
 @socketio.on('move_container')
 def on_move_container(data):
     room_id = data.get('roomId')
-    container_id = data.get('container_id')
+    container_id = data.get('containerId')
     x = data.get('x')
     y = data.get('y')
 
-    if not room_id or container_id is None or x is None or y is None:
+    if not room_id or not container_id:
         return
+
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        return
+
 
     emit(
         'container_moved', {
-            'container_id' : container_id,
+            'containerId' : container_id,
             'x' :x,
             'y': y,
         },
