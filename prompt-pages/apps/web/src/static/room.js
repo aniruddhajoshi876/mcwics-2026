b10fc488efa6804ca58eb5e5fc3042352b8a2a63
ilgaz <ilgazalsancak2006@gmail.com>
2026-02-07 22:53:37 -0500

    updated post creation

--- prompt-pages/apps/web/src/static/room.js
+++ prompt-pages/apps/web/src/static/room.js
@@ -1,8 +1,29 @@
 const addButtonEl = document.querySelector(".add-post-js");
 
+const postSpawnEl = document.querySelector('.post-spawn-js');
+
 console.log("Running!")
+console.log(Date.now())
 
 addButtonEl.addEventListener('click', () => {
     console.log("Clicked!")
+
+    const postHTML = `    <div id="box1" class="box" draggable="true">
+        <span class="name" contenteditable="true">user</span>
+        <span class="date" id="date"></span>
+
+        <div class="image"> 
+            <input type="file">     
+        </div>
+        <div class="caption" contenteditable="true">edit me</div>
+    </div>
+    `
+
+    postSpawnEl.insertAdjacentHTML('beforeend', postHTML);
+
+    const newPost = postSpawnEl.lastElementChild;
+
+    newPost.id = 'post_' + Date.now();
+    newPost.ondragstart = drag_start;
 
 })
