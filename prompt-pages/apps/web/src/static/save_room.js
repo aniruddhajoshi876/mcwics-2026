802bfaa204c2ce8cfbe31d3f7b327273311dad55
ilgaz <ilgazalsancak2006@gmail.com>
2026-02-08 09:34:23 -0500

    worked on save function

--- prompt-pages/apps/web/src/static/save_room.js
+++ prompt-pages/apps/web/src/static/save_room.js
@@ -10,7 +10,18 @@ saveEl.addEventListener('click', () => {
 });
 
 function saveRoom() {
-    const posts = postsDiv.children;
-    console.log(posts);
-
+    console.log(postsDiv)
+    postsDiv.forEach(post => {
+        const caption = post.querySelector('.caption').innerText;
+        const name = post.querySelector('.name').innerText;
+        const date = post.querySelector('.date').innerText;
+        const id = post.querySelector('.box').id;
+        const top = post.querySelector('.box').style.top;
+        const left = post.querySelector('.box').style.left; 
+        
+        console.log(post.innerHTML);
+        console.log(caption, name, date, id, top, left);
+    }
+    //console.log(posts.innerHTML);
+)
 }
